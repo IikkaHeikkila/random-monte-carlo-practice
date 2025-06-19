33af4fa417a54b2c6fc2ff7a60264247d51bf957
IikkaHeikkila <78319873+IikkaHeikkila@users.noreply.github.com>
2025-06-19 19:39:49 +0300

    Add files via upload

---
+++ Task_1.R
@@ -0,0 +1,49 @@
+library(adaptMCMC)
+library(coda)
+
+objective_function <- function(x){
+	-1*((1-x[1])^2 + 100*(x[2]-x[1]^2)^2)
+}
+
+set.seed(66)
+x0 <- c(-1,1)
+niter <- 20000
+scale <- c(0.01, 0.01)
+
+mcmc_with_adaptation <- MCMC(objective_function, init=x0, n=niter)
+
+mcmc_without_adaptation <- MCMC(objective_function, init=x0, 
+					scale=scale, n=niter, adapt=FALSE)
+
+#---Trajectories
+#plot(mcmc_with_adaptation$samples, dim = 2, type="l", main="Trajectories")
+xn <- mcmc_with_adaptation$samples[nrow(mcmc_with_adaptation$samples),]
+points(x0[1], x0[2], col = "red", pch = 16, cex = 2)
+points(xn[1], xn[2], col = "blue", pch = 16, cex = 2)
+xn
+-objective_function(xn)
+
+#---Coda stuff
+mcmc <- as.mcmc(matrix(mcmc_with_adaptation$samples, ncol=2))
+#traceplot(mcmc, main="Trace")
+#plot(autocorr(mcmc), main = "Autocorr", type="l", xlab="delay")
+
+#---Heidelberger-Welch
+heidel.diag(mcmc)
+
+
+#---Trajectories
+#plot(mcmc_without_adaptation$samples, dim = 2, type="l", main="Trajectories")
+xn <- mcmc_without_adaptation$samples[nrow(mcmc_without_adaptation$samples),]
+points(x0[1], x0[2], col = "red", pch = 16, cex = 2)
+points(xn[1], xn[2], col = "blue", pch = 16, cex = 2)
+xn
+-objective_function(xn)
+
+#---Coda stuff
+mcmc <- as.mcmc(matrix(mcmc_without_adaptation$samples, ncol=2))
+#traceplot(mcmc, main="Trace")
+#plot(autocorr(mcmc), main = "Autocorr", type="l", xlab="delay")
+
+#---Heidelberger-Welch
+heidel.diag(mcmc)
---
+++ Task_2.R
@@ -0,0 +1,12 @@
+
+library("BMA")
+set.seed(666)
+file_path <- "test.txt"
+data <- read.table(file_path)
+df <- data.frame(data)
+y <- df[,7]
+X <- cbind(df[,3:6], df[,8:21]) #Discard index variable and nominal variable
+niter <- 100000
+
+model <- MC3.REG(y, X, num.its = niter, outliers=FALSE, K=5)
+summary(model)
