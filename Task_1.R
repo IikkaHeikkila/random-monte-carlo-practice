fcfc1a4cc09fede335720399bc7c9e4cd6d6845b
IikkaHeikkila <78319873+IikkaHeikkila@users.noreply.github.com>
2025-06-19 19:29:45 +0300

    Add files via upload

---
+++ Task_1.R
@@ -0,0 +1,80 @@
+normm<-function (Nsim, a) 
+{
+        vec <- vector("numeric", Nsim)
+        x <- 0 #I think that this is 0 because dnorm
+        vec[1] <- x #First value to zero
+	  count <- 0 #Setting count to zero for acceptance rate
+        for (i in 2:Nsim) { 
+                innov <- runif(1, -a, a) #innov from unif distribution
+                Xstar <- x + innov #Adding "noise" x and putting that to Xstar
+                aprob <- min(1, dnorm(Xstar)/dnorm(x)) #Calculating acceptance probability
+                u <- runif(1)
+                if (u < aprob){ #Checking acceptance
+                        x <- Xstar #Updating x
+				count <- count+1 #Count++
+		    }
+                vec[i] <- x #x_i to vec_i
+        }
+	  rate <- count/(Nsim-1) #Calculating acceptance rate
+	  list(vec=vec, rate=rate) #Returns
+}
+
+set.seed(33)
+Nsim <- 2000
+a <- 55	#Tune here!
+result <- normm(Nsim, a)
+print(result$rate)
+plot(result$vec, type = "l", xlab = "Iteration", 
+	ylab = "Value", main = sprintf("a = %.1f", a))
+
+hist(result$vec, breaks = 30, col = "blue", 
+xlab = "Value", ylab = "Freq", main = sprintf("a = %.1f", a))
+
+
+#---------------------------------------------------------------
+
+
+gammh<-function (Nsim, a, b) #MH algorithm for gamma distribution
+{
+        mu <- a/b		#Mean of gamma
+        sig <- sqrt(a/(b * b))	#Deviation of gamma
+        vec <- vector("numeric", Nsim)
+        x <- a/b
+        vec[1] <- x
+        for (i in 2:Nsim) {
+             can <- rnorm(1, mu, sig) #From standard distribution
+		 #Point here below is that if can is more dense in gamma and in norm,
+		 #then there is a high propability to choose the second elemt of min()
+		 #instead of 1
+             aprob <- min(1, (dgamma(can, a, b)/dgamma(x, 
+             a, b))/(dnorm(can, mu, sig)/dnorm(x,mu, sig)))
+                u <- runif(1) #U from uniformal dist.
+                if (u < aprob) #Checking acceptance
+                        x <- can #Updating x
+                vec[i] <- x #save to vec
+        }
+        vec #return
+}
+set.seed(33)
+Nsim = 2000
+a = 2				#Modify!
+b = 2				#Modify!
+vec = gammh(Nsim, a, b)
+
+plot(vec, type = "l", xlab = "Iteration", 
+	ylab = "Value", main = sprintf("a = %.2f, b = %.2f", a, b))
+
+hist(vec, breaks = 30, col = "blue", 
+xlab = "Value", ylab = "Freq", main = sprintf("a = %.2f, b = %.2f", a, b))
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ Task_2.R
@@ -0,0 +1,67 @@
+
+
+inv <- function(X){
+	EV <- eigen(X)
+	EV$vector%*%diag(1/EV$values)%*%t(EV$vector)
+}
+
+
+lpostw <- function(gam, y, X, beta){
+	n <- length(y)
+	qgam <- sum(gam)
+	Xt1 <- cbind(rep(1,n), X[,which(gam==1)])
+	if(gam!=0){
+		P1 <- Xt1%*%inv(t(Xt1)%*%Xt1)%*%t(Xt1)
+	}
+	else{
+		P1 <- matrix(0,n,n)
+	}
+	-(qgam+1)/2*log(n+1)-n/2*log(t(y)%*%y-n/(n+1)*t(y)%*%P1%*%y-1/(n+1)*t(beta)%*%t(cbind(rep(1,n),X))%*%P1%*%cbind(rep(1,n),X)%*%beta)
+}
+
+gocho <- function(niter, y, X){
+	lga <- dim(X)[2]
+	beta <- lm(y ~ ., data = df <- data.frame(y, X))$coeff
+	y <- as.matrix(y)
+	X <- as.matrix(X)
+	gamma <- matrix(0, nrow=niter, ncol=lga)
+	gamma[1,] <- sample(c(0,1), lga, rep=T)
+	for(t in 1:(niter-1)){
+		j <- sample(1:lga, 1)
+		gam0=gam1=gamma[t,];gam1[j]=1-gam0[j]
+		pr <- lpostw(gam0, y, X, beta)
+		pr <- c(pr, lpostw(gam1, y, X, beta))
+		pr <- exp(pr-max(pr))
+		gamma[t+1,] <- gam0
+		if(sample(c(0,1), 1, prob=pr)){
+			gamma[t+1,]=gam1
+		}
+	}
+	gamma
+}
+
+set.seed(33)
+file_path <- "test.txt"
+data <- read.table(file_path)
+df <- data.frame(data)
+y <- df[,7]
+X <- cbind(df[,3:6], df[,8:21])
+niter <- 25000
+gamma <- gocho(niter, y, X)
+gamma <- gamma[5001:niter, ] #BURN!
+apply(gamma, 2, mean)
+
+prob_of_model <- rep(0, ncol(gamma))
+for (i in 1:ncol(gamma)) {
+  prob_of_model[i] <- sum(gamma[, i]) / nrow(gamma)
+}
+prob_sorted <- sort(prob_of_model, decreasing = TRUE)
+prob_sorted
+top_5 <- prob_sorted[1:5]
+print(top_5)
+
+
+
+
+
+
